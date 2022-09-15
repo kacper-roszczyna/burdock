3c349988a1224a82a108e1fa97b092774646cd02
Kacper Roszczyna <kacper.roszczyna@gmail.com>
2022-09-15 22:22:03 +0200

    initial commit

---
+++ src/main/java/com/roszck/burdock/BurdockApplication.java
@@ -0,0 +1,13 @@
+package com.roszck.burdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurdockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurdockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roszck/burdock/BurdockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roszck.burdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurdockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
